2540329c4f4a0e6c232d780d72c535dd97aeda1f
SibylGao <gaoshiyu@ict.ac.cn>
2022-03-22 18:43:45 +0800

    Update test-data.py

--- test-data.py
+++ test-data.py
@@ -1,11 +1,11 @@
-# with open("F:\\ubiquant\\ubiquant-market-prediction\\train.csv") as f:
-#     for line in f:
-#         print(line)
+with open("F:\\ubiquant\\ubiquant-market-prediction\\train.csv") as f:
+    for line in f:
+        print(line)
 
-from fileinput import filename
-import pickle
+# from fileinput import filename
+# import pickle
 
-filename = "F:\\ubiquant\\ubiquant-market-prediction\\train.csv"
-save_object = "F:\\ubiquant\\ubiquant-market-prediction\\train.pkl"
-with open(filename,'w') as f:
-    pickle.dump(save_object,f)
+# filename = "F:\\ubiquant\\ubiquant-market-prediction\\train.csv"
+# save_object = "F:\\ubiquant\\ubiquant-market-prediction\\train.pkl"
+# with open(filename,'w') as f:
+#     pickle.dump(save_object,f)
